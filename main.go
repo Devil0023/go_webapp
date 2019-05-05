3c12f3fc9648e78d9fc7eb5f0321589746ba2115
zhang_ll <zhang_ll@yoozoo.com>
2019-05-05 20:20:39 +0800

    [WN] Use gin

--- main.go
+++ main.go
@@ -1,20 +1,10 @@
 package main
 
 import (
+	"./pkg/setting"
 	"fmt"
-	"io/ioutil"
 )
 
-var env string
-
 func main() {
-	info, err := ioutil.ReadFile("./.env")
-
-	if err != nil {
-		fmt.Println("Can't Read Env: %s\n", err)
-		panic(err)
-	}
-
-	env = string(info[:])
-
+	fmt.Println(setting.HttpPort)
 }
--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -1,16 +1,56 @@
 package setting
 
 import (
-	"fmt"
 	"github.com/go-ini/ini"
+	"io/ioutil"
+	"log"
+	"time"
 )
 
 var (
 	Cfg *ini.File
+
+	Env     string
+	RunMode string
+
+	HttpPort int
+
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+
+	JwtSecret string
 )
 
 func init() {
 
-	//Cfg, err = ini.load("conf/app." + env +".ini")
-	fmt.Println(env)
+	info, err := ioutil.ReadFile("./.env")
+
+	if err != nil {
+		log.Fatal("Failed to parse env: %s\n", err)
+	}
+
+	Env = string(info[:])
+
+	Cfg, err = ini.Load("conf/app." + Env + ".ini")
+	if err != nil {
+		log.Fatal(2, "Failed to parse ini: %v", err)
+	}
+
+	LoadRunMode()
+	LoadHttpServer()
+	LoadApp()
+}
+
+func LoadRunMode() {
+	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("debug")
+}
+
+func LoadHttpServer() {
+	HttpPort = Cfg.Section("server").Key("HTTP_PORT").MustInt(80)
+	ReadTimeout = time.Duration(Cfg.Section("server").Key("READ_TIMEOUT").MustInt(60))
+	WriteTimeout = time.Duration(Cfg.Section("server").Key("Write_TIMEOUT").MustInt(60))
+}
+
+func LoadApp() {
+	JwtSecret = Cfg.Section("app").Key("JWT_SECRET").MustString("")
 }
