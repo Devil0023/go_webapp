159a96bac13441546c2f000f1a717091508ed3e7
zhang_ll <zhang_ll@yoozoo.com>
2019-05-06 16:13:10 +0800

    [WN] 分组路由

---
+++ routers/api/v1/tag.go
@@ -0,0 +1,19 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+func GetTags(context *gin.Context) {
+
+}
+
+func AddTag(context *gin.Context) {
+
+}
+
+func EditTag(context *gin.Context) {
+
+}
+
+func DeleteTag(context *gin.Context) {
+
+}
--- routers/router.go
+++ routers/router.go
@@ -2,18 +2,22 @@ package routers
 
 import "github.com/gin-gonic/gin"
 import "../pkg/setting"
+import "./api/v1"
 
 func InitRouter() *gin.Engine {
-	route := gin.New()
-	route.Use(gin.Logger())
-	route.Use(gin.Recovery())
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
 
-	route.GET("/test", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	apiv1 := router.Group("/api/v1")
 
-	return route
+	{
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.POST("/tags", v1.AddTag)
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	}
+
+	return router
 }
