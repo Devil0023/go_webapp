6a2520da6cda1ec718d9036ad211185ecfee0a7c
zhanglei <zhang_ll@yoozoo.com>
2019-07-06 14:39:11 +0800

    [WN] 修改log

--- pkg/logging/file.go
+++ pkg/logging/file.go
@@ -23,13 +23,15 @@ func openLogFile(filePath string) *os.File {
 
 	case os.IsNotExist(err):
 		os.MkdirAll(dir+setting.LogSavePath, os.ModePerm)
+		break
 
 	case os.IsPermission(err):
 		log.Fatalf("Permission : %v", err)
+		break
 
 	}
 
-	handle, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	handle, err := os.OpenFile(dir+filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
 		log.Fatalf("Failed to open file : %v", err)
