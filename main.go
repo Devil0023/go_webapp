00b762e5fccf0f38d733ff69a93d0e05e1cf63ae
zhanglei <zhang_ll@yoozoo.com>
2019-07-08 10:27:30 +0800

    [WN] 修改重启

--- main.go
+++ main.go
@@ -4,18 +4,28 @@ import (
 	"./pkg/setting"
 	"./routers"
 	"fmt"
-	"net/http"
+	"github.com/fvbock/endless"
+	"log"
+	"syscall"
 )
 
 func main() {
 
-	server := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HttpPort),
-		Handler:        routers.InitRouter(),
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+
+	endPoint := fmt.Sprintf(":%d", setting.HttpPort)
+
+	server := endless.NewServer(endPoint, routers.InitRouter())
+
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Server Error: $v", err)
+	}
 }
